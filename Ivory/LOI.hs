8c87a7cc9bcec5dfef4aae2e47aea9e378c1ea39
leepike <leepike@gmail.com>
2014-09-11 22:23:40 -0400

    packing, unpacking cucsauth code typechecks.

--- Ivory/LOI.hs
+++ Ivory/LOI.hs
@@ -16,22 +16,42 @@
 
 module LOI where
 
-import Prelude hiding (return)
+import Prelude hiding (return, init)
 import Ivory.Language
+import Ivory.Serialize
 
--- foo :: Def ('[] :-> ())
--- foo = proc "foo" $ body $ retVoid
+-- memcpy :: Def ('[ IDouble
+--                 , ConstRef s2 (CArray (Stored Uint8))
+--                 , Sint32] :-> Sint32)
+-- memcpy = importProc "memcpy" "string.h"
 
+-- sizeOf :: Def ('[IDouble] :-> Sint32)
+-- sizeOf = proc "sizeof" $ \d -> body $ do undefined
+
+-- packDouble :: Def ('[IDouble, Ref s (CArray (Stored Uint8))] :-> ())
+-- packDouble = proc "packDouble" $ \d ref -> body $ do undefined
+
+foo :: Def ('[Ref s (Array 576 (Stored Uint8)), Uint32] :-> Sint32)
+foo = proc "foo" $ \buf ix -> body $ do
+  v <- unpack (constRef (toCArray buf)) ix
+  ret v
+
+
+-- type Buf = Array 576 (Stored Uint8)
+type Idx = Ix 576
+
+-- For CucsAuthorisationRequest idd field
 [ivory|
-void foo()
-{
-  if(true) {}
-  else {return;}
-}
-
+string struct LOI 10
 |]
 
+packIxAdd ix0 ix1 = ix0 + twosComplementRep (fromIx ix1)
 
+[ivoryFile|packing.ivory|]
+[ivoryFile|CucsAuthorisationRequest.ivory|]
+-- [ivoryFile|loi.ivory|]
 
-
--- [ivoryFile|src/loi.ivory|]
+-- loiModule :: Module
+-- loiModule = package "loiModule" $ do
+--   depend packing
+  -- memcpy
